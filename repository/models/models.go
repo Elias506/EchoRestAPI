910b8ee9abbd9cd566af1815f94a0d4dad14af77
Elias506 <elias506@yandex.ru>
2021-02-11 18:39:58 +0300

    Update rep/models

--- repository/models/models.go
+++ repository/models/models.go
@@ -1,7 +1,8 @@
-package repository
+package models
 
 import (
 	. "github.com/elias506/EchoRestAPI/models"
+	"os"
 )
 
 type IUserDB interface {
@@ -10,6 +11,11 @@ type IUserDB interface {
 	DeleteUser(id int) (int, error)
 	UpdateUser(id int, reqUser *RequestUser) (int, error)
 	AddUser(reqUser *RequestUser) (int, error)
+}
+
+type FileDB struct {
+	Path string
+	File *os.File
 }
 
 func IGetUser(i IUserDB, id int) (*User, error) {
